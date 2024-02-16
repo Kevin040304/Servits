5da1461ee3c427f687b582de0878cf4241c8f8c4
Kevin040304 <137554858+Kevin040304@users.noreply.github.com>
2024-02-16 18:42:42 -0500

    en el registro cree ya la opcion de agregar datos para la base de datos usando javascript y node js, haciendo que el registro ya se completamente funcional

--- mysql.js
+++ mysql.js
@@ -1,15 +1,52 @@
-const mysql = require ('mysql')
-const connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-database: 'demo',
-password: ''
+const express = require("express");
+
+const mysql= require("mysql");
+
+const app = express();
+
+
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'demo',
+    password: ''
 })
 
-connection.connect((err)=>{
-if(err) throw err
-console.log('Conexion establecida exitosamenter')
+app.set ("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extend:false}));
+
+app.get("/", function(req,res){
+    res.render("registro");
 })
 
+app.post("/validar", function(req,res){
+    const datos = req.body;
 
-connection.end()
+    let nombre = datos.nombre;
+    let contrasena = datos.contrasena;
+    let confirmarContrasena = datos.confirmarContrasena;
+
+    let registrar = "INSERT INTO users(username, password)VALUES ('"+nombre+"','"+contrasena+"')";
+
+    conexion.query(registrar, function(error){
+        if(error){
+            throw error;
+        }else{
+            console.log("datos almacenados correctamente");
+
+        }
+    });
+
+    
+
+    console.log(datos);
+});
+
+
+app.listen(3000, function(){
+    console.log("Servidor creado http://localhost:3000");
+})
